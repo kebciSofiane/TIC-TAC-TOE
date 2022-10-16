ee428f14e54e711220372e64e27e911710a95bb0
33749 <sofyanekebci@gmail.com>
2022-10-16 11:51:40 +0200

    code simplified, win system created, everything's working

--- App/main.py
+++ App/main.py
@@ -10,35 +10,12 @@ screenHeight = 500
 
 gameScreenHeight = 500
 gameScreenWidth = 590
-cellWidth = 590 / 3
-cellHeight = 500 / 3
 
-cell1_1X = 0
-cell1_1Y = 0
+cellWidth = gameScreenWidth = 590 / 3
+cellHeight = gameScreenHeight / 3
 
-cell1_2X = cell1_1X + cellWidth
-cell1_2Y = cell1_1Y
-
-cell1_3X = cell1_2X + cellWidth
-cell1_3Y = cell1_2Y
-
-cell2_1X = cell1_1X
-cell2_1Y = cell1_1Y + cellHeight
-
-cell2_2X = cell1_1X + cellWidth
-cell2_2Y = cell2_1Y
-
-cell2_3X = cell1_2X + cellWidth
-cell2_3Y = cell2_1Y
-
-cell3_1X = cell1_1X
-cell3_1Y = cell2_1Y + cellHeight
-
-cell3_2X = cell1_2X
-cell3_2Y = cell3_1Y
-
-cell3_3X = cell1_3X
-cell3_3Y = cell3_1Y
+menuWidth = 200
+menuHeight = 480
 
 surface = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Tic-Tac-Toe")
@@ -54,7 +31,7 @@ imgO = pygame.image.load("D:\\France\\Programmation\\pyhton\\Images\\R.png")
 imgX = pygame.transform.scale(imgX, (150, 150))
 imgO = pygame.transform.scale(imgO, (150, 150))
 
-rect = pygame.draw.rect(surface, "white", pygame.Rect((screenWidth - 210, screenHeight - 490, 200, 480)))
+rect = pygame.draw.rect(surface, "white", pygame.Rect((screenWidth - 210, screenHeight - 490, menuWidth, menuHeight)))
 font = pygame.font.Font('freesansbold.ttf', 20)
 
 textStart = font.render('Start', True, "black", "red")
@@ -67,45 +44,62 @@ textQuitDisplay = surface.blit(textQuit, (654, 80))
 
 pygame.display.flip()
 
-boxUsed = ["", "", "", "", "", "", "", "", ""]
+boxUsed = [["", "", ""], ["", "", ""], ["", "", ""]]
 
 
-def XorO(cellY, cellX, i):
+def drawXorO(row, col):
     global clickState
-    if boxUsed[i] == "":
+    print(row, "-", col)
+    if boxUsed[row][col] == "":
         if clickState == "X":
-            surface.blit(imgX, (cellX, cellY))
+            surface.blit(imgX, (cellWidth * col, cellHeight * row))
             clickState = "O"
-            boxUsed[i] = "X"
+            boxUsed[row][col] = "X"
         else:
-            surface.blit(imgO, (cellX, cellY))
+            surface.blit(imgO, (cellWidth * col, cellHeight * row))
             clickState = "X"
-            boxUsed[i] = "O"
-
+            boxUsed[row][col] = "O"
+        checkWin(row, col)
         pygame.display.update()
+
+
+def checkWin(row, col):
+    if boxUsed[row][0] == boxUsed[row][1] == boxUsed[row][2]:
+        print('It''s a win')
+    elif boxUsed[0][col] == boxUsed[1][col] == boxUsed[2][col]:
+        print('It''s a win')
+    elif boxUsed[0][0] == boxUsed[1][1] == boxUsed[2][2] == "X":
+        print('It''s a win')
+    elif boxUsed[0][0] == boxUsed[1][1] == boxUsed[2][2] == "O":
+        print('It''s a win')
+    elif boxUsed[2][0] == boxUsed[1][1] == boxUsed[0][2] == "X":
+        print('It''s a win')
+    elif boxUsed[2][0] == boxUsed[1][1] == boxUsed[0][2] == "O":
+        print('It''s a win')
+
+
+def findColAndRow(x, y):
+    if x < cellWidth:
+        col = 0
+    elif x < cellWidth * 2:
+        col = 1
+    else:
+        col = 2
+
+    if y < cellHeight:
+        row = 0
+    elif y < cellHeight * 2:
+        row = 1
+    else:
+        row = 2
+
+    drawXorO(row, col)
 
 
 def mouseClickDetection():
     if event.type == pygame.MOUSEBUTTONDOWN:
         (x, y) = pygame.mouse.get_pos()
-        if cell1_1X + cellWidth > x > cell1_1X and cell1_1Y + cellHeight > y > cell1_1Y:
-            XorO(cell1_1Y, cell1_1X, 0)
-        elif cell1_2X + cellWidth > x > cell1_2X and cell1_2Y + cellHeight > y > cell1_2Y:
-            XorO(cell1_2Y, cell1_2X, 1)
-        elif cell1_3X + cellWidth > x > cell1_3X and cell1_3Y + cellHeight > y > cell1_3Y:
-            XorO(cell1_3Y, cell1_3X, 2)
-        elif cell2_1X + cellWidth > x > cell2_1X and cell2_1Y + cellHeight > y > cell2_1Y:
-            XorO(cell2_1Y, cell2_1X, 3)
-        elif cell2_2X + cellWidth > x > cell2_2X and cell2_2Y + cellHeight > y > cell2_2Y:
-            XorO(cell2_2Y, cell2_2X, 4)
-        elif cell2_3X + cellWidth > x > cell2_3X and cell2_3Y + cellHeight > y > cell2_3Y:
-            XorO(cell2_3Y, cell2_3X, 5)
-        elif cell3_1X + cellWidth > x > cell3_1X and cell3_1Y + cellHeight > y > cell3_1Y:
-            XorO(cell3_1Y, cell3_1X, 6)
-        elif cell3_2X + cellWidth > x > cell3_2X and cell3_2Y + cellHeight > y > cell3_2Y:
-            XorO(cell3_2Y, cell3_2X, 7)
-        elif cell3_3X + cellWidth > x > cell3_3X and cell3_3Y + cellHeight > y > cell3_3Y:
-            XorO(cell3_3Y, cell3_3X, 8)
+        findColAndRow(x, y)
 
 
 while running:
